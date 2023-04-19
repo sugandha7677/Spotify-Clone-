6abd9940af0c93f5f56df73d8b8383b624852090
Sugandha <sugandhatiwary7677@gmail.com>
2023-04-19 14:10:34 +0530

    c

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
-import "./output.css";
-import LoginComponent from "./routes/Login";
+// import "./output.css";
+// import LoginComponent from "./routes/Login";
 
 
-// import {BrowserRouter, Routes, Route } from "react-router-dom";
-// npm instal
-function App() {
-  return (
-    <div className="w-screen h-screen">
-    <BrowserRouter>
+// // import {BrowserRouter, Routes, Route } from "react-router-dom";
+// // npm instal
+// function App() {
+//   return (
+//     <div className="w-screen h-screen">
+//     <BrowserRouter>
  
-    <Routes>
-      <Route path = "/" element = {<HelloComponent />}/>
-      <Route path ="/login" element = {<LoginComponent/>} />
-    </Routes>
+//     <Routes>
+//       <Route path = "/" element = {<HelloComponent />}/>
+//       <Route path ="/login" element = {<LoginComponent/>} />
+//     </Routes>
     
-    </BrowserRouter>
-    </div>
-  );
-}
+//     </BrowserRouter>
+//     </div>
+//   );
+// }
 
-const HelloComponent = () => {
-  return <div>This is hello from component</div>;
-};
+// const HelloComponent = () => {
+//   return <div>This is hello from component</div>;
+// };
 
-export default App;
+// export default App;
